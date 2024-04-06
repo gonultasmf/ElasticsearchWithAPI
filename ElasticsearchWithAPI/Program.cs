73aa41b854b7eb296058a4584479d6c63fc95350
Mustafa Gönültaş <gonultasmf@hotmail.com>
2024-04-06 10:03:29 +0300

    Update Program.cs

--- ElasticsearchWithAPI/Program.cs
+++ ElasticsearchWithAPI/Program.cs
@@ -14,35 +14,6 @@ builder.Services.AddSwaggerGen();
 configureLogging();
 builder.Host.UseSerilog();
 
-//var configuration = builder.Configuration;
-//var environment = builder.Environment;
-
-//#region Serilog
-
-//Log.Logger = new LoggerConfiguration()
-//    .Enrich.FromLogContext()
-//    .Enrich.WithMachineName()
-//    .WriteTo.Debug()
-//    .WriteTo.Console()
-//    .WriteTo.File("log.txt", rollingInterval: RollingInterval.Day)
-//    .WriteTo.Elasticsearch(ConfigureElasticSink(configuration, environment.EnvironmentName))
-//    .Enrich.WithProperty("Environment", environment.EnvironmentName)
-//    .ReadFrom.Configuration(configuration)
-//    .CreateLogger();
-
-
-//static ElasticsearchSinkOptions ConfigureElasticSink(IConfigurationRoot configuration, string environment)
-//{
-//    return new ElasticsearchSinkOptions(new Uri(configuration["ElasticConfiguration:Uri"]))
-//    {
-//        AutoRegisterTemplate = true,
-//        IndexFormat = $"{Assembly.GetExecutingAssembly().GetName().Name.ToLower().Replace(".", "-")}-{environment?.ToLower().Replace(".", "-")}-{DateTime.UtcNow:yyyy-MM}"
-//    };
-//}
-
-//builder.Services.AddSerilog(Log.Logger);
-//#endregion
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
